05a005a5261933d181819fc1aa2803f9f4ef4383
Matthias <matze7x@googlemail.com>
2021-04-30 10:08:53 +0200

    Kommentiert Umrandungsbot

--- src/main/java/Umrandungsbot.java
+++ src/main/java/Umrandungsbot.java
@@ -1,5 +1,8 @@
 import robocode.*;
 
+/**
+ * Ein Roboter, der am Rande des Spielfelds lang fährt und in die Mitte schießt
+ */
 public class Umrandungsbot extends AdvancedRobot {
 
     boolean moveForward = true;
@@ -7,13 +10,16 @@ public class Umrandungsbot extends AdvancedRobot {
     boolean first = true;
     int turnDirection = -1; //1 equals clockwise and -1 equals counterclockwise
 
+    /**
+     * Hauptmethode des Roboters
+     */
     public void run() {
         setTurnLeft(getHeading()%90);
         setTurnGunLeft(90);
-        setAhead(2000);
+        setAhead(getBattleFieldHeight()+getBattleFieldWidth());
 
         while (true) {
-            if (getHeading()%90 != 0){
+            if (getHeading()%90 != 0){      //Korrigiert die Fahrtrichtung auf einen rechten Winkel
                 setTurnLeft(getHeading()%90);
             }
             if (moveBackward) {
@@ -25,6 +31,10 @@ public class Umrandungsbot extends AdvancedRobot {
         }
     }
 
+    /**
+     * Wenn der Roboter von einer Kugel getroffen wurde, dreht er seine Fahrtrichtung um
+     * @param event - HitByBulletEvent
+     */
     public void onHitByBullet(HitByBulletEvent event) {
         if (!first) {
             stop();
@@ -41,6 +51,10 @@ public class Umrandungsbot extends AdvancedRobot {
         }
     }
 
+    /**
+     * Wenn der Roboter eine Wand trifft, dreht er sich und seine Kanone
+     * @param event
+     */
     public void onHitWall(HitWallEvent event) {
         if (turnDirection==1){
             turnRight(90);
@@ -50,6 +64,10 @@ public class Umrandungsbot extends AdvancedRobot {
         first = false;
     }
 
+    /**
+     * Wenn der Roboter einen Roboter sieht schießt er
+     * @param e
+     */
     public void onScannedRobot(ScannedRobotEvent e) {
         fire(1);
     }
